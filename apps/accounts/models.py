f4903d1ea1ed0432e37614b6b0bc2568fbf833a3
leumars-o <samex150@gmail.com>
2025-09-06 14:30:18 -0100

    core model architechture for accounts added
    Signed-off-by: leumars-o <samex150@gmail.com>

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Account(models.Model):
+    email = models.EmailField(unique=True)
+    wallet_address = models.CharField(max_length=64)
+    is_active = models.BooleanField(default=True)
+    is_excluded_from_tracking = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=0)
+    current_streak = models.IntegerField(default=0)
+    total_earnings = models.DecimalField(max_digits=20, decimal_place=6)
