2ac07a4efbc09abe2eb75bdf4bc59b3211fd64d2
leumars-o <samex150@gmail.com>
2025-09-08 00:47:37 +0100

    wallet monitoring service logic structure added

---
+++ apps/wallet_monitoring/services.py
@@ -0,0 +1,12 @@
+class WalletMonitoringService:
+    def get_wallet_balance(wallet_address):
+        pass
+
+    def calculate_expected_balance(account):
+        pass
+
+    def check_balance_discrepancy(account):
+        pass
+
+    def record_balance_log(account, calculated, actual):
+        pass
