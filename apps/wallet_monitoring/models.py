fb4d0cd1ce620b09b4a4b08e1f5e2f7a81dc42ac
leumars-o <samex150@gmail.com>
2025-09-07 20:25:18 -0100

    core model architechture for wallet monitoring  added
    Signed-off-by: leumars-o <samex150@gmail.com>

--- apps/wallet_monitoring/models.py
+++ apps/wallet_monitoring/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class BalanceLog(models.Model):
+    account = models.ForeignKey(Account)
+    calculated_balance = models.DecimalField(max_digits=20, decimal_places=6)
+    actual_balance = models.DecimalField(max_digits=20, decimal_places=6)
+    discrepancy = models.DecimalField(max_digits=20, decimal_places=6)
+    checked_at = models.DateTimeField(auto_now_add=True)
+
+class ManualTransaction(models.Model):
+    TRANSACTION_TYPES = [
+        ('DEPOSIT', 'Manual Deposit'),
+        ('WITHDRAWAL', 'Manual Withdrawal'),
+    ]
+    
+    account = models.ForeignKey(Account)
+    amount = models.DecimalField(max_digits=20, decimal_places=6)
+    transaction_type = models.CharField(max_length=20, choices=TRANSACTION_TYPES)
+    note = models.TextField()
+    recorded_at = models.DateTimeField(auto_now_add=True)
