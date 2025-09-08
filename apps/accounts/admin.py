ca1b26bfeab45e294e247130840d7a22bbb6532e
leumars-o <samex150@gmail.com>
2025-09-08 01:46:55 +0100

    added admin files for all apps

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Account
 
-# Register your models here.
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ['email', 'wallet_address', 'is_active', 'current_streak', 'total_earnings']
+    list_filter = ['is_active', 'is_excluded_from_tracking']
+    search_fields = ['email', 'wallet_address']
+    
